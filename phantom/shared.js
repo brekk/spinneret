6e657e16a896759ebc9e42b88665d297f437bc15
brekk <brekk@brekkbockrath.com>
2025-02-20 20:21:44 -0800

    nix phantom

--- phantom/shared.js
+++
@@ -1,9 +0,0 @@
-import { pipe, replace } from "ramda"
-
-export const entities = pipe(
-  replace(/\s"(\S)/g, ` &ldquo;$1`),
-  replace(/^"(\S)/g, `&ldquo;$1`),
-  replace(/(\S)"\s/g, `$1&rdquo; `),
-  replace(/(\S)"$/g, `$1&rdquo;`),
-  replace(/\.\.\./g, `&hellip;`),
-)
